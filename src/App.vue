9e30a6f621bf3ca32af2f2cbe5e967ba5802548e
HZKroy <2676475186@qq.com>
2020-10-24 18:23:30 +0800

    write your meaaage

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div id="app">
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default{
+  name:'app',
+  components:{
+    
+  }
+}
+</script>
+
+<style>
+</style>
